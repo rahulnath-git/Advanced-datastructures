3b759d6ef060f962c1471175d6c7a7cbd58cadf7
prajwal-56 <prajwalpraveen2006@gmail.com>
2026-01-31 16:47:36 +0530

    refactored the rotation selection part , and some bug fixed (Still contains bugs)

--- prajwal/avl.py
+++ prajwal/avl.py
@@ -11,10 +11,12 @@ class AVLTree:
         print("a new tree created")
 
     def insert(self, value , node:Node=None , isFirstCall=False):
-        
+
         if self.root is None:       # if the tree is just Initialized (No root node exist)
             self.root = Node(value)
             return self.root
+        
+        # print(f"type of left node : {type(node)}")
         
         if  node is None and not isFirstCall:                         # just a falg to indicate whether this is the first time calling the method
             self.root = self.insert(value=value , node=self.root , isFirstCall=True )
@@ -30,19 +32,34 @@ class AVLTree:
         else:
             return node
         
+        print(f"the value {value} has been inserted to the node {node} ")
         # updating the height & balance factor after insertion
         # max() - because the tallest is what determines the height of the node 
         node.height = 1 + max( self.getHeight(node.left) , self.getHeight(node.right))
         balanceFactor = self.getBalanceStatus(node)
 
         # Selecting which rotation to perform   
-        # if balanceFactor > 1:
-        #     # this means , the structure is a zig-zag (<)
-        #     thatVal = node.left
-        #     node.left = self.le
+        if balanceFactor > 1:
+            if self.getBalanceStatus(node.left) >=0:
+                print("A L-L case has found")            
+                return self.rightRot(node) # LL
+            else: 
+                print("A L-R case has found")
+                node.left = self.leftRot(node.left)
+                return self.rightRot(node)  # LR 
 
 
-
+        if balanceFactor < -1:
+            if self.getBalanceStatus(node.right) <= 0: # RR 
+                print("A R-R case has found")
+                return self.leftRot(node)
+            else:
+                print("A R-L case has found")
+                node.right = self.rightRot(node.right)
+                return self.leftRot(node)
+            
+        return node
+            
     # types of rotation :
     #  * Left-Left (LL) -> single Right Rotation
     #       z            y
@@ -72,6 +89,7 @@ class AVLTree:
 
 
     def rightRot(self , z:Node ):  # A single right rotation 
+        print("A right rotation has performed")
         y:Node = z.left
         t = y.right
 
@@ -79,25 +97,28 @@ class AVLTree:
         y.right = z
         z.left = t 
 
-        print("a right-right rotation just happend")
-
         # updating the heights - for finding the the actual height after the rotation
         z.height = 1 + max(self.getHeight(z.left) ,self.getHeight(z.right))
         y.height = 1 + max(self.getHeight(y.left) ,self.getHeight(y.right))
 
+        print("A right rotation is being performed")
         return y
 
     def leftRot(self , z:Node ): 
+        print("A left rotation is being performed")
+
         y:Node = z.right
         t = y.left
 
         # the actual Rotation
         y.left = z
-        z.left = t
+        z.right = t
 
         # updating the height
-        z.height = 1 + max( self.getHeight(z.left) - self.getHeight(z.right)) 
-        y.height = 1 + max( self.getHeight(y.left) - self.getHeight(y.right)) 
+        z.height = 1 + max( self.getHeight(z.left) , self.getHeight(z.right)) 
+        y.height = 1 + max( self.getHeight(y.left) , self.getHeight(y.right)) 
+
+        print("a left rotation has been performed")
     
         return y
     def getHeight(self , node:Node):
@@ -111,8 +132,13 @@ class AVLTree:
             return 0
         balanceFactor = self.getHeight(node.left) - self.getHeight(node.right)
         return balanceFactor
-    
+
 tree0 = AVLTree()
-tree0.insert(56 )
-tree0.insert(523 )
-tree0.insert(12 )
+0
+tree0.insert(value=12)
+tree0.insert(value=10)
+tree0.insert(value=56)
+tree0.insert(value=50)
+tree0.insert(value=122)
+tree0.insert(value=2)
+# print(type(tree0.))'
